fbd12d64d456f9df682147cf1c28fee0c7e68937
ROLIKA <rolikaagarwal2003@gmail.com>
2023-03-10 12:14:52 +0530

    modified

--- js/main.js
+++ js/main.js
@@ -111,15 +111,3 @@ items.forEach((element) => {
     document.querySelector('.drop4').style.display = 'none';
   });
 });
-//TALK WITH EXPERT STARTS
-function fun() {
-  const openFormBtn = document.getElementById('form-open');
-  const formContainer = document.querySelector('.form-container');
-
-  openFormBtn.addEventListener('click', function () {
-    window.location.href = 'form.html';
-    window.scrollTo(0, document.body.scrollHeight);
-  });
-}
-
-//TALK WITH EXPERT ENDS
---
+++ js/talkwithexpert.js
@@ -0,0 +1,13 @@
+//TALK WITH EXPERT STARTS
+function fun() {
+    const openFormBtn = document.getElementById('form-open');
+    const formContainer = document.querySelector('.form-container');
+  
+    openFormBtn.addEventListener('click', function () {
+      window.location.href = 'form.html';
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }
+  
+  //TALK WITH EXPERT ENDS
+  
