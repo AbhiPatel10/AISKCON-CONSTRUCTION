884040845b65a230e44c492477c02086263dd5d5
Kartikay Asija <kartikay7057@gmail.com>
2023-03-17 02:26:58 +0530

    testimonial

--- js/main.js
+++ js/main.js
@@ -79,3 +79,36 @@ items.forEach((element)=>{
     });
 });
 // Navbar end
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    breakpoints: {  
+        '480': {
+          slidesPerView: 1,
+        },
+        '640': {
+          slidesPerView: 2,
+        },
+        '900':{
+            slidesPerView: 3,
+        },
+        '1430':{
+            slidesPerView: 4,
+        }
+    },
+    // spaceBetween: 0,
+    // slidesPerGroup: 3,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // autoplay: {
+    //     // delay: 2000,
+    // },
+});
