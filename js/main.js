d682c6f567d52e8a5f558e00867330b3766278c6
Kartikay Asija <kartikay7057@gmail.com>
2023-05-11 02:23:02 +0530

    collage added

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ faqQuestions.forEach((question) => {
 if(document.querySelector('.footer-copyright')){
   let year = new Date();
   year = year.getFullYear();
-  document.querySelector('.footer-copyright').innerHTML = 'Copyright &#169; ' + year + ' <a href="/">Aiskcon</a>, All Right Reserved';
+  document.querySelector('.footer-copyright').innerHTML = 'Copyright &#169; ' + year + ' <a href="./">Aiskcon</a>, All Rights Reserved';
 }
 
 // Particle Js
